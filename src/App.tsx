8d5bb7aabc6f8c68afeb6f87dcd8bb4c0843cd4e
Marcos Volke <marcos@sizex.com.br>
2020-07-19 14:45:14 -0300

    Configuração da status bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => (
+  <>
+    {/* só no android tem algumas opções a mais na status bar como backroundColor */}
+    {/* a opção translucent deixa a barra transparente usando o fundo do app, a hidden esconde */}
+    <StatusBar barStyle="light-content" backgroundColor="#312e38"  />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+);
 
 export default App;
