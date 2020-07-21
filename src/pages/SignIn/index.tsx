5d93ff810d2aa9c468a748ecf47cc23b75cf7b4f
Marcos Volke <marcos@sizex.com.br>
2020-07-21 08:06:54 -0300

    Mais dicas para deixar a tela melhor para o usuário

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, Image, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/input';
@@ -23,20 +23,27 @@ const SignIn: React.FC = () => {
         behavior={Platform.OS === 'ios' ? 'padding': undefined}
         enabled
       >
-        <Container>
-          <Image source={logoImg} />
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
 
-          <Title>Faça seu logon</Title>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-          <Input name="email" icon="mail" placeholder="E-mail" />
-          <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-          <Button onPress={() => {}}>Entrar</Button>
+            <Button onPress={() => {}}>Entrar</Button>
 
-          <ForgotPassword onPress={() => {}}>
-            <ForgotPasswordText>Esqueci Minha Senha</ForgotPasswordText>
-          </ForgotPassword>
-        </Container>
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci Minha Senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
       </KeyboardAvoidingView>
 
       <CreateAccountButton onPress={() => {}}>
