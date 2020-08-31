9d7580229ff6494a876eeccfe23df729096717f6
Marcos Volke <marcos@sizex.com.br>
2020-08-31 09:21:41 -0300

    Tela de Agendamento Concluido

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
+
+  const handleOkPressed = useCallback(() => {
+    // o reset refaz o historico de navegacao pra nao permitir usar o voltar
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        }
+      ],
+      index: 0,
+    })
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h",
+      { locale: ptBR });
+  }, [routeParams.date, format]);
+
   return (
-    <View />
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento Concluído</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   );
 }
 
